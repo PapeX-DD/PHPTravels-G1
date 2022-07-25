f1eae6e968fcedb5cd0f134c6498a5aae55beb6f
JDICorleone <88350442+JDICorleone@users.noreply.github.com>
2022-07-25 12:37:58 -0600

    Create main.py
    Creación del Main, creación de la función "timingTest" que mide los tiempos de respuesta del performance del back-end y front-end.

---
+++ main.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+
+
+source = 'https://phptravels.net/' #URL
+PATH = 'C:\Chrome_driver\chromedriver.exe' #Ubicación del WebDriver
+driver = webdriver.Chrome(PATH)
+driver.get(source)
+
+
+def timingTest():
+        #Este calcula dos tiempos o timings.
+        #Performance de back-end: desde cuando el usuario empieza la navegación hasta cuando recibe la primera respuesta.
+        #Performance del front-end: inicia cuando el usuario recibe la primera respuesta y hasta que la carga del DOM se complete.
+       
+        navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
+        responseStart = driver.execute_script("return window.performance.timing.responseStart")
+        domComplete = driver.execute_script("return window.performance.timing.domComplete")
+
+        backendPerformance = responseStart - navigationStart
+        frontendPerformance = domComplete - responseStart
+
+        print ('Calculando tiempos de respuesta')
+        print ('Tiempo del Back End: ' + str(backendPerformance) +' ms')
+        print ('Tiempo del Front End: ' + str(frontendPerformance)+' ms')
+
+        driver.quit()
+
+timingTest()
+
+
+
