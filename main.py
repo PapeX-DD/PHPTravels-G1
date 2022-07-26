824b965785ddebebb09ae37a501dc8dd87bc477c
JDICorleone <88350442+JDICorleone@users.noreply.github.com>
2022-07-25 22:36:09 -0600

    Update main.py
    Comentario más corto

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ import time
 import warnings
 
 def timingTest(driver):
-        # Este calcula dos tiempos o timings.
-        # Performance de back-end: desde cuando el usuario empieza la navegación hasta cuando recibe la primera respuesta.
-        # Performance del front-end: inicia cuando el usuario recibe la primera respuesta y hasta que la carga del DOM se complete.
-       
+        # Este calcula los tiempos o timings del performance del back-end y del front-end.
         navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
         responseStart = driver.execute_script("return window.performance.timing.responseStart")
         domComplete = driver.execute_script("return window.performance.timing.domComplete")
